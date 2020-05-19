c4914263169c5290c651ac56250606d03ab1e386
Vinicius Crisol <61323173+ViniciusCrisol@users.noreply.github.com>
2020-05-19 11:12:32 -0300

    commiy

--- src/database/index.js
+++ src/database/index.js
@@ -6,14 +6,11 @@ class Database {
   }
 
   mongo() {
-    this.mongoConnection = mongoose.connect(
-      'mongodb+srv://Crisol01:327fdvcs2@cluster0-0z65f.gcp.mongodb.net/test?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: true,
-      }
-    );
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    });
   }
 }
 
